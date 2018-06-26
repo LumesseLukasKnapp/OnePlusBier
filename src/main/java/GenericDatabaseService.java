671d7166cb7ec43cb01afddce250da363ba3133c
Stefan <123@123.de>
2018-06-26 09:42:34 +0200

    Added Search with HQL

--- src/main/java/GenericDatabaseService.java
+++ src/main/java/GenericDatabaseService.java
@@ -3,6 +3,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
 import java.util.ArrayList;
 
@@ -13,23 +14,25 @@ public class GenericDatabaseService<T> implements IGenericDatabaseService<T> {
 
     private SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 
-    public <T> T get(Class<T> type,  int id) {
+    public <T> T get(Class<T> type, int id) {
         Session session = sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
+
         Object result = session.get(type, id);
+
         transaction.commit();
         session.close();
         return (T) result;
-        //return sessionFactory.getCurrentSession().get(type,id);
     }
 
     public <T> ArrayList<T> getAll(Class<T> type) {
         Session session = sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
+
         Criteria criteria = session.createCriteria(type);
         transaction.commit();
-
         ArrayList<T> resultList = (ArrayList<T>) criteria.list();
+
         session.close();
         return resultList;
     }
@@ -37,7 +40,9 @@ public class GenericDatabaseService<T> implements IGenericDatabaseService<T> {
     public <T> void save(T entity) {
         Session session = sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
+
         session.saveOrUpdate(entity);
+
         transaction.commit();
         session.close();
     }
@@ -45,8 +50,22 @@ public class GenericDatabaseService<T> implements IGenericDatabaseService<T> {
     public <T> void delete(T entity) {
         Session session = sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
+
         session.delete(entity);
+
         transaction.commit();
         session.close();
+    }
+
+    public <T> ArrayList<T> search(String HQL) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        Query query = session.createQuery(HQL);
+        ArrayList<T> result = (ArrayList<T>) query.list();
+
+        transaction.commit();
+        session.close();
+        return result;
     }
 }
